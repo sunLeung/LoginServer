306ccf5203ff5a7915bfa6e3a24d1d2021afa039
liangyx <liangyuxin.02@gmail.com>
2014-07-24 21:59:50 +0800

    Common development.

---
+++ LoginServer/src/bean/Player.java
@@ -0,0 +1,83 @@
+package bean;
+
+import java.sql.Timestamp;
+
+public class Player extends Pojo{
+	private int loginid;
+	private String name;
+	private String ip;
+	private String email;
+	private String phone;
+	private String unionid;
+	private String password;
+	private Timestamp createTime;
+	private String model;
+	private String mark;
+	private Timestamp lastLoginTime;
+	public int getLoginid() {
+		return loginid;
+	}
+	public void setLoginid(int loginid) {
+		this.loginid = loginid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getUnionid() {
+		return unionid;
+	}
+	public void setUnionid(String unionid) {
+		this.unionid = unionid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getMark() {
+		return mark;
+	}
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	public Timestamp getLastLoginTime() {
+		return lastLoginTime;
+	}
+	public void setLastLoginTime(Timestamp lastLoginTime) {
+		this.lastLoginTime = lastLoginTime;
+	}
+}
---
+++ LoginServer/src/dao/LoginDao.java
@@ -0,0 +1,69 @@
+package dao;
+
+import static common.db.DbUtils.dbUtils;
+
+import java.sql.SQLException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import bean.Player;
+
+public class LoginDao {
+
+	private static Log log=LogFactory.getLog(LoginDao.class);
+	
+	public static int save(Player bean){
+		try {
+			return dbUtils.insert(bean);
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	
+	public static int update(Player bean){
+		try {
+			return dbUtils.update(Player.class, "where loginid=?", bean.getLoginid());
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	
+	public static Player getPlayerByLoginid(int loginid){
+		Player bean=null;
+		try {
+			bean=dbUtils.read(Player.class, "where loginid=?", loginid);
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return bean;
+	}
+	
+	public static Player getPlayerByPhone(String phone){
+		Player bean=null;
+		try {
+			bean=dbUtils.read(Player.class, "where phone=?", phone);
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return bean;
+	}
+	
+	public static Player getPlayerByEmail(String email){
+		Player bean=null;
+		try {
+			bean=dbUtils.read(Player.class, "where email=?", email);
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return bean;
+	}
+
+}
